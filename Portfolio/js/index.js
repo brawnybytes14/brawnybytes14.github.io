4e312aa9f54dc337c724229c74e647de7018d8b9
Rohit Kori <rohitkori00@gmail.com>
2018-08-02 22:46:24 +0530

    Update index.js

--- Portfolio/js/index.js
+++ Portfolio/js/index.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!','ROHIT' ],
+			strings: [ 'HAPPY', 'BIRTHDAY!','MISS GUNJAN' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
